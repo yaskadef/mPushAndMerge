3ee88269eaa97671c58bd4695b955d666f9763d3
yaskadefPersonal <osnkalyashov@mail.ru>
2025-12-09 17:27:42 +0300

    Implemented PlayerPrefsGameDataProvider

--- Assets/mPushAndMerge/Scripts/Game/Data/PlayerPrefsGameDataProvider.cs
+++ Assets/mPushAndMerge/Scripts/Game/Data/PlayerPrefsGameDataProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.mPushAndMerge.Scripts.Game.Data.Root;
 using Assets.mPushAndMerge.Scripts.Game.Data.Root.Maps;
+using Newtonsoft.Json;
 using R3;
 using System;
 using System.Collections.Generic;
@@ -19,7 +20,10 @@ namespace Assets.mPushAndMerge.Scripts.Game.Data
             if (PlayerPrefs.HasKey(GAME_DATA_KEY))
             {
                 var json = PlayerPrefs.GetString(GAME_DATA_KEY);
+                _gameDataOrigin = JsonConvert.DeserializeObject<GameData>(json);
+                GameData = new GameDataProxy(_gameDataOrigin);
 
+                Debug.Log($"GameData loaded from PlayerPrefs: {json}");
             }
             else
             {
@@ -34,12 +38,14 @@ namespace Assets.mPushAndMerge.Scripts.Game.Data
 
         public void ResetData()
         {
-            throw new NotImplementedException();
+            GameData = GetGameDataFromSettings();
+            SaveGameData();
         }
 
         public void SaveGameData()
         {
-            throw new NotImplementedException();
+            var json = JsonConvert.SerializeObject(_gameDataOrigin);
+            PlayerPrefs.SetString(GAME_DATA_KEY, json);
         }
 
         private GameDataProxy GetGameDataFromSettings()
