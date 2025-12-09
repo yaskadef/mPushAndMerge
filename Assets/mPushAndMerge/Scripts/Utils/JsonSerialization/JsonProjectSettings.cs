55935ba6ca9d8bf9db35b04d555e5cfeac4e84a0
yaskadefPersonal <osnkalyashov@mail.ru>
2025-12-09 13:33:26 +0300

    added Newtonsoft.Json and default settings customized

---
+++ Assets/mPushAndMerge/Scripts/Utils/JsonSerialization/JsonProjectSettings.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.mPushAndMerge.Scripts.Utils.JsonSerialization
+{
+    public class JsonProjectSettings
+    {
+        public static void ApplyJsonEntityConverterSettings()
+        {
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new JsonEntityConverter());
+
+            JsonConvert.DefaultSettings = () => settings;
+        }
+    }
+}
